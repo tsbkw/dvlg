113d9ceea83558d5833ebc2065a98584e8d63544
tsbkw <tsbkw0@gmail.com>
2019-06-20 08:39:04 +0900

    setup circleci

--- local_docker_test.sh
+++ local_docker_test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-rm -r ./dist/*
+if [ -n "$(ls dist/*)" ]; then rm -r ./dist/*; fi
 yarn generate
+mkdir docker/dist
+cp -R dist/* docker/dist
 docker stop dvlg
 docker-compose -f ./docker/docker-compose.yml build
 docker-compose -f ./docker/docker-compose.yml up --d
+rm -R docker/dist/*
