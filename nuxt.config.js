87f093e83ce9fbdf5570c44ea457dd8e853cb6cb
tsbkw <tsbkw0@gmail.com>
2019-06-19 09:29:27 +0900

    add OGP meta tag to all pages, and some page dependent tags in markdown page

--- nuxt.config.js
+++ nuxt.config.js
@@ -103,11 +103,20 @@ export default {
   ** Headers of the page
   */
   head: {
+    htmlAttrs: {
+      prefix: 'og: http://ogp.me/ns#'
+    },
     title: pkg.name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      { hid: 'og:site_name', property: 'og:site_name', content: dvlgConfig.ogpSiteName },
+      { hid: 'og:type', property: 'og:type', content: 'artice' },
+      { hid: 'og:url', property: 'og:url', content: dvlgConfig.ogpBaseUrl },
+      { hid: 'og:title', property: 'og:title', content: dvlgConfig.ogpSiteName },
+      { hid: 'og:description', property: 'og:description', content: dvlgConfig.ogpBaseDescription },
+      { hid: 'og:image', property: 'og:image', content: `${dvlgConfig.ogpBaseUrl}/icon.png` }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -187,7 +196,8 @@ export default {
     author: dvlgConfig.author,
     contact: dvlgConfig.contact,
     github: dvlgConfig.github,
-    firstPublishYear: dvlgConfig.firstPublishedYear
+    firstPublishYear: dvlgConfig.firstPublishedYear,
+    baseUrl: dvlgConfig.ogpBaseUrl
   },
 
   markdownit: {
